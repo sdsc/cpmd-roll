8e58b4a9dabc25b037ae72dd48a0d89c69df838a
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 10:09:15 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/cpmd/version.mk
+++ src/cpmd/version.mk
@@ -1,3 +1,14 @@
-NAME    = cpmd_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 3.15.3
-RELEASE = 1
+NAME               = cpmd_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 3.15.3
+RELEASE            = 1
+PKGROOT            = /opt/cpmd
+
+SRC_SUBDIR         = cpmd
+
+SOURCE_NAME        = cpmd
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME).$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)

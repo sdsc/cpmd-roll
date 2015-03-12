53a6e5260e35a50d1b1b44faf24906d64c5d0e30
Jerry Greenberg <jpg@comet-devel.local>
2015-03-12 10:34:11 -0700

    add auto versioning

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME = cpmd
-RELEASE  = 0
-COLOR    = orange
+ROLLNAME        = cpmd
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
 
-REDHAT.ROOT = $(PWD)
+REDHAT.ROOT     = $(PWD)

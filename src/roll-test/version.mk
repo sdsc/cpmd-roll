e59ecf8164735ae709f008688016ee9fffa1f940
Jerry Greenberg <jpg@sdsc.edu>
2019-01-17 17:22:00 -0800

    APS-980 cpmd 4.3; added cuda option

--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,7 +1,13 @@
 NAME       = sdsc-cpmd-roll-test
 VERSION    = 1
-RELEASE    = 2
+RELEASE    = 3
 PKGROOT    = /root/rolltests
+
+CUDAVER=
+ifneq ("$(ROLLOPTS)", "$(subst cuda=,,$(ROLLOPTS))")
+  CUDAVER = $(subst cuda=,,$(filter cuda=%,$(ROLLOPTS)))
+endif
+
 
 RPM.EXTRAS = AutoReq:No\nAutoProv:No
 RPM.FILES  = $(PKGROOT)/cpmd.t
